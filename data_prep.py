b07d2571087d70668a68bbfc5de1a71f1143c396
jtfwong <43226692+jtfwong@users.noreply.github.com>
2018-11-22 13:49:47 -0700

    Phase_1 file prep
    First part for file prep (terms.txt) is not complete. The other outputs are fine

---
+++ data_prep.py
@@ -0,0 +1,40 @@
+import xml.etree.ElementTree as ET
+
+def main():
+    file = input('Enter name of file to be prepped: ')
+    tree = ET.parse(file)
+    root = tree.getroot()
+#    f1 = open("terms.txt", 'w+')
+#    f1.close()
+
+    f2 = open("pdates.txt", 'w+')
+    for ad in list(root):
+        date = ad.find('date').text
+        aid = ad.find('aid').text
+        cat = ad.find('cat').text
+        loc = ad.find('loc').text
+        f2.write('%s:%s,%s,%s\n' % (date, aid, cat, loc))
+    f2.close()
+
+    f3 = open("prices.txt", 'w+')
+    for ad in list(root):
+        price = ad.find('price').text
+        aid = ad.find('aid').text
+        cat = ad.find('cat').text
+        loc = ad.find('loc').text
+        f3.write('%s:%s,%s,%s\n' % (price, aid, cat, loc))
+    f3.close()
+
+    f4 = open("ads.txt", 'w+')
+    for ad in list(root):
+        aid = ad.find('aid').text
+        date = ad.find('date').text
+        loc = ad.find('loc').text
+        cat = ad.find('cat').text
+        ti = ad.find('ti').text
+        desc = ad.find('desc').text
+        price = ad.find('price').text
+        f4.write('%s:%s,%s,%s,%s,%s,%s\n' % (aid, date, loc, cat, ti, desc, price))
+    f4.close()
+
+main()
