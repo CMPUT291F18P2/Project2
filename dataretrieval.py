2e992d4345911a4e637bde058fb791b344becf56
William Wong <williamwong@Williams-MacBook-Air.local>
2018-11-26 14:58:27 -0700

    bug fixing

--- dataretrieval.py
+++ dataretrieval.py
@@ -78,7 +78,7 @@ def grammar(input,outputFormat):
             elif re.match(catQuery,expr) != None:
                 resultid = set(catagoryCheck(re.findall(cat,expr)[1]))
             id = id & resultid
-            print(id)
+            print(list(id))
         ads = adSearch(id,outputFormat)
         for ad in ads:
             print(ad)
